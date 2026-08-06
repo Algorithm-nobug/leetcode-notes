29cb6675b5a7dfd243b500c8b9eb15d3a9183074
Algorithm-nobug <3561460182@qq.com>
2026-08-06 14:29:27 +0800

    Add files via upload

---
+++ No.2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>hashtable;
+        for(auto& i:strs){
+            string key=i;
+            sort(key.begin(),key.end());
+            hashtable[key].push_back(i);
+        }
+        vector<vector<string>>ans;
+        for(auto& j:hashtable){
+            ans.push_back(j.second);
+        }
+        return ans;
+    }
+};
