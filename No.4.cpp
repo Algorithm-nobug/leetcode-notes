82da50ebfeeb2aed99b5316233913178af7fe970
Algorithm-nobug <3561460182@qq.com>
2026-08-07 14:54:16 +0800

    Add files via upload

---
+++ No.4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l=0,r=0;
+        while(r<nums.size()){
+            if(nums[r]){
+                swap(nums[l],nums[r]);
+                l++;
+            }
+            r++;
+        }
+    }
+};
+//˫ָ�뷨 
